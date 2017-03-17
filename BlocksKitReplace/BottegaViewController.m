6c8c0c00b993c7060b441c2f8bf2a5e5b8f960ab
mike <mike@loilo.tv>
2017-03-17 10:20:45 +0900

    NSArray+BlocksKitをNSArrayの拡張で代替した。

--- BlocksKitReplace/BottegaViewController.m
+++ BlocksKitReplace/BottegaViewController.m
@@ -25,13 +25,20 @@
 //    self.navigationItem.rightBarButtonItem = [UIBarButtonItem.alloc initWithTitle:@"NULL" style:UIBarButtonItemStylePlain handler:NULL];
     
     NSArray *array = @[@"a", @"b", @"c"];
-    array = [array lbk_mapWithBlock:^id(id obj) {
-        if ([obj isEqualToString:@"b"])
-            return @"d";
-        else
-            return obj;
-    }];
-    NSLog(@"ARRAY : %@", array);
+//    array = [array lbk_mapWithBlock:^id(id obj) {
+//        if ([obj isEqualToString:@"b"])
+//            return @"d";
+//        else
+//            return obj;
+//    }];
+//    array = [array lbk_selectWithBlock:^BOOL(id obj) {
+//        return [obj isEqualToString:@"c"];
+//    }];
+//    array = [array lbk_rejectWithBlock:^BOOL(id obj) {
+//        return [obj isEqualToString:@"c"];
+//    }];
+//    NSLog(@"ARRAY : %@", array);
+    NSLog(@"ARRAY ANY : %@", [array lbk_anyWithBlock:^BOOL(id obj) { return [obj isEqualToString:@"a"]; }] ? @"YES" : @"NO");
 }
 
 - (void)didReceiveMemoryWarning {
--- BlocksKitReplace/Replaced/NSArray+Util.swift
+++ BlocksKitReplace/Replaced/NSArray+Util.swift
@@ -9,12 +9,33 @@
 import UIKit
 
 extension NSArray {
+    
+    public func lbk_match(block: @escaping (AnyObject) -> Bool) -> AnyObject? {
+        let index = self.indexOfObject(passingTest:) { (obj, idx, stop) -> Bool in
+            return block(obj as AnyObject)
+        }
+        if index == NSNotFound {
+            return nil
+        }
+        return self[index] as AnyObject?
+    }
+    
+    public func lbk_select(block: @escaping (AnyObject) -> Bool) -> NSArray {
+        return self.objects(at: self.indexesOfObjects(passingTest:) { (obj, idx, stop) -> Bool in
+            return block(obj as AnyObject)
+        }) as NSArray
+    }
+    
+    public func lbk_reject(block: @escaping (AnyObject) -> Bool) -> NSArray {
+        return self.lbk_select(block: { (obj) -> Bool in
+            return !block(obj as AnyObject)
+        })
+    }
 
     public func lbk_map(block: (AnyObject) -> (AnyObject?)) -> NSArray {
         let result = NSMutableArray(capacity: self.count)
         self.enumerateObjects({ (obj, idx, stop) in
             let value = block(obj as AnyObject)
-            NSLog("\(obj) -> \(value)")
             if nil == value {
                 result.add(NSNull())
             }
@@ -23,5 +44,9 @@ extension NSArray {
             }
         })
         return result
+    }
+    
+    public func lbk_any(block: @escaping (AnyObject) -> Bool) -> Bool {
+        return self.lbk_match(block: block) != nil
     }
 }
