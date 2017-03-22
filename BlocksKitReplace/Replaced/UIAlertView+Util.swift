2434d62b54077de6a8575922157b4c166f6bac99
mike <mike@loilo.tv>
2017-03-22 12:32:42 +0900

    SwiftだとUIAlertViewの挙動に問題があるため、UIAlertViewの拡張ではあるものの、UIAlertControllerで実装するようにした。

--- BlocksKitReplace/Replaced/UIAlertView+Util.swift
+++ BlocksKitReplace/Replaced/UIAlertView+Util.swift
@@ -8,100 +8,87 @@
 
 import UIKit
 
-var UIAlertViewDisableFirstOtherButtonKey: UInt8 = 0
 var UIAlertViewCallbackKey: UInt8 = 0
 var UIAlertViewTextInputCallbackKey: UInt8 = 0
 typealias UIAlertViewCallback = @convention(block) (_ sender: AnyObject, _ buttonIndex: NSInteger) -> ()
 typealias UIAlertViewTextInputCallback = @convention(block) (_ sender: AnyObject, _ buttonIndex: NSInteger, _ text: String?) -> ()
 
-extension UIAlertView: UIAlertViewDelegate {
+/** 元々、UIAlertViewを拡張して、UIAlertControllerを扱えるようにしていたが、
+ * Swiftで実装すると、ボタンの有効化を遅延させたり、入力用のテキストボックスを開いたりするときにうまくいかない
+ * iOS8以降はUIAlertView自体が非推奨になっていることもあり、UIAlertViewでは実装しない
+ *
+ * [問題]
+ * SwiftだとalertViewShouldEnableFirstOtherButtonが呼ばれないみたいなので、キャンセルボタン以外を一時的に無効にしておく処理が実現できない。
+ * 仮にalertViewShouldEnableFirstOtherButtonが呼ばれるようになったとしても、ボタンを有効に戻すために一度閉じて開き直す処理が効かない。
+ * 入力用のテキストボックスに関しては、alertViewStyleに.secureTextInputや.plainTextInputを設定しているにも関わらず表示されない。
+ */
+extension UIAlertView {
 
-    /** UIAlertControllerが使えるかどうか */
-    class func controllerIsActive() -> Bool {
-        return nil != NSClassFromString("UIAlertController")
-    }
-    
-    /** UIAlertControllerを優先的に使う、メッセージを表示するだけのUIAlertView */
-    class func lbk_show(presenter: UIViewController?, title: String?, message: String?, buttonTitle: String?) -> AnyObject {
+    /** メッセージを表示するだけのUIAlertController */
+    class func lbk_show(presenter: UIViewController, title: String?, message: String?, buttonTitle: String?) -> AnyObject {
         return self.lbk_show(presenter: presenter, title: title, message: message, buttonTitle: buttonTitle, callback: nil)
     }
-    class func lbk_show(presenter: UIViewController?, title: String?, message: String?, buttonTitle: String?, callback: (() -> ())?) -> AnyObject {
-        if self.controllerIsActive() && nil != presenter {
-            let alert = UIAlertController(title:title, message: message, preferredStyle: .alert)
-            alert.addAction(UIAlertAction(title: buttonTitle, style: .default, handler: { (action) in
-                if nil != callback { callback!() }
-            }))
-            presenter!.present(alert, animated: true, completion: nil)
-            return alert
-        }
-        else {
-            let alert = UIAlertView(title: title, message: message, delegate: nil, cancelButtonTitle: buttonTitle)
-            alert.lbk_callback = { (sender, buttonIndex) in
-                if nil != callback { callback!() }
-            }
-            alert.delegate = alert
-            alert.show()
-            return alert
-        }
+    class func lbk_show(presenter: UIViewController, title: String?, message: String?, buttonTitle: String?, callback: (() -> ())?) -> AnyObject {
+        let alert = UIAlertController(title:title, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: buttonTitle, style: .default, handler: { (action) in
+            if nil != callback { callback!() }
+        }))
+        presenter.present(alert, animated: true, completion: nil)
+        return alert
     }
 
-    /** UIAlertControllerを優先的に使う、cancel/other2つボタンのUIAlertView */
-    class func lbk_show(presenter: UIViewController?, title: String?, message: String?, cancelButtonTitle: String?, otherButtonTitle: String?, callback: ((_ sender: AnyObject, _ buttonIndex: NSInteger) -> ())?) -> AnyObject {
+    /** cancel/other2つボタンのUIAlertController */
+    class func lbk_show(presenter: UIViewController, title: String?, message: String?, cancelButtonTitle: String?, otherButtonTitle: String?, callback: ((_ sender: AnyObject, _ buttonIndex: NSInteger) -> ())?) -> AnyObject {
         return self.lbk_show(presenter: presenter, title: title, message: message, cancelButtonTitle: cancelButtonTitle, otherButtonTitles: nil != otherButtonTitle ? [otherButtonTitle!] : nil, delayActiveTime: 0, callback: callback)
     }
-    class func lbk_show(presenter: UIViewController?, title: String?, message: String?, cancelButtonTitle: String?, otherButtonTitles: [String]?, callback: ((_ sender: AnyObject, _ buttonIndex: NSInteger) -> ())?) -> AnyObject {
+    class func lbk_show(presenter: UIViewController, title: String?, message: String?, cancelButtonTitle: String?, otherButtonTitles: [String]?, callback: ((_ sender: AnyObject, _ buttonIndex: NSInteger) -> ())?) -> AnyObject {
         return self.lbk_show(presenter: presenter, title: title, message: message, cancelButtonTitle: cancelButtonTitle, otherButtonTitles: otherButtonTitles, delayActiveTime: 0, callback: callback)
     }
     
-    /** UIAlertControllerを優先的に使う、otherButtonを一定時間後に有効にするUIAlertView */
-    class func lbk_show(presenter: UIViewController?, title: String?, message: String?, cancelButtonTitle: String?, otherButtonTitle: String?, delayActiveTime: TimeInterval, callback: ((_ sender: AnyObject, _ buttonIndex: NSInteger) -> ())?) -> AnyObject {
+    /** otherButtonを一定時間後に有効にするUIAlertController */
+    class func lbk_show(presenter: UIViewController, title: String?, message: String?, cancelButtonTitle: String?, otherButtonTitle: String?, delayActiveTime: TimeInterval, callback: ((_ sender: AnyObject, _ buttonIndex: NSInteger) -> ())?) -> AnyObject {
         return self.lbk_show(presenter: presenter, title: title, message: message, cancelButtonTitle: cancelButtonTitle, otherButtonTitles: nil != otherButtonTitle ? [otherButtonTitle!] : nil, delayActiveTime: delayActiveTime, callback: callback)
     }
-    class func lbk_show(presenter: UIViewController?, title: String?, message: String?, cancelButtonTitle: String?, otherButtonTitles: [String]?, delayActiveTime: TimeInterval, callback: ((_ sender: AnyObject, _ buttonIndex: NSInteger) -> ())?) -> AnyObject {
-        if self.controllerIsActive() && nil != presenter {
-            let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
-            weak var w = alert
-            alert.addAction(UIAlertAction(title: cancelButtonTitle, style: .default, handler: { (action) in
-                guard let strong = w else { return }
-                if nil != callback { callback!(strong, 0) }
-            }))
-            var loginActions: [UIAlertAction] = []
-            if nil != otherButtonTitles {
-                for otherButtonTitle in otherButtonTitles! {
-                    let index = otherButtonTitles!.index(of: otherButtonTitle)!
-                    let loginAction = UIAlertAction(title: otherButtonTitle, style: (1 == otherButtonTitles!.count ? .destructive : .default), handler: { (action) in
-                        guard let strong = w else { return }
-                        if nil != callback { callback!(strong, index + 1) }
-                    })
-                    alert.addAction(loginAction)
-                    loginActions.append(loginAction)
-                }
-            }
-            if 0 < loginActions.count && 0 < delayActiveTime {
-                for loginAction in loginActions {
-                    loginAction.isEnabled = false
-                }
-                presenter!.present(alert, animated: true, completion: {
-                    DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + Double(Int64(delayActiveTime * Double(NSEC_PER_SEC))) / Double(NSEC_PER_SEC), execute: {
-                        if nil == w { return }
-                        for loginAction in loginActions {
-                            loginAction.isEnabled = true
-                        }
-                    })
+    class func lbk_show(presenter: UIViewController, title: String?, message: String?, cancelButtonTitle: String?, otherButtonTitles: [String]?, delayActiveTime: TimeInterval, callback: ((_ sender: AnyObject, _ buttonIndex: NSInteger) -> ())?) -> AnyObject {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        weak var w = alert
+        alert.addAction(UIAlertAction(title: cancelButtonTitle, style: .default, handler: { (action) in
+            guard let strong = w else { return }
+            if nil != callback { callback!(strong, 0) }
+        }))
+        var loginActions: [UIAlertAction] = []
+        if nil != otherButtonTitles {
+            for otherButtonTitle in otherButtonTitles! {
+                let index = otherButtonTitles!.index(of: otherButtonTitle)!
+                let loginAction = UIAlertAction(title: otherButtonTitle, style: (1 == otherButtonTitles!.count ? .destructive : .default), handler: { (action) in
+                    guard let strong = w else { return }
+                    if nil != callback { callback!(strong, index + 1) }
                 })
+                alert.addAction(loginAction)
+                loginActions.append(loginAction)
             }
-            else {
-                presenter!.present(alert, animated: true, completion: nil)
+        }
+        if 0 < loginActions.count && 0 < delayActiveTime {
+            for loginAction in loginActions {
+                loginAction.isEnabled = false
             }
-            return alert
+            presenter.present(alert, animated: true, completion: {
+                DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + Double(Int64(delayActiveTime * Double(NSEC_PER_SEC))) / Double(NSEC_PER_SEC), execute: {
+                    if nil == w { return }
+                    for loginAction in loginActions {
+                        loginAction.isEnabled = true
+                    }
+                })
+            })
         }
         else {
-            return UIAlertView(title: title, message: message, cancelButtonTitle: cancelButtonTitle, otherButtonTitles: otherButtonTitles, delayActiveTime: delayActiveTime, callback: callback)
+            presenter.present(alert, animated: true, completion: nil)
         }
+        return alert
     }
     
     /** UIAlertControllerを優先的に使う、テキスト入力UIAlertView */
-    class func lbk_showTextInput(presenter: UIViewController?, title: String?, message: String?, cancelButtonTitle: String?, otherButtonTitle: String?, text: String?, placeholder: String?, secureTextEntry: Bool, keyboardType: UIKeyboardType, limitation: UInt, callback: ((_ sender: AnyObject, _ buttonIndex: NSInteger, _ text: String?) -> ())?) -> AnyObject {
+    class func lbk_showTextInput(presenter: UIViewController, title: String?, message: String?, cancelButtonTitle: String?, otherButtonTitle: String?, text: String?, placeholder: String?, secureTextEntry: Bool, keyboardType: UIKeyboardType, limitation: UInt, callback: ((_ sender: AnyObject, _ buttonIndex: NSInteger, _ text: String?) -> ())?) -> AnyObject {
         // UIAlertViewでのテキスト入力中に文字数制限をするため、UITextFieldTextDidChangeNotificationで実現する
         weak var _textField: UITextField? = nil
         NotificationCenter.default.addObserver(forName: NSNotification.Name.UITextFieldTextDidChange, object: nil, queue: nil) { (note) in
@@ -119,79 +106,28 @@ extension UIAlertView: UIAlertViewDelegate {
             }
         }
         
-        if self.controllerIsActive() {
-            let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
-            weak var w = alert
-            alert.addAction(UIAlertAction(title: cancelButtonTitle, style: .default, handler: { (action) in
-                guard let strong = w else { return }
-                if nil != callback { callback!(strong, 0, nil) }
-            }))
-            alert.addAction(UIAlertAction(title: otherButtonTitle, style: .default, handler: { (action) in
-                guard let strong = w else { return }
-                if nil != callback { callback!(strong, 1, strong.textField?.text) }
-            }))
-            alert.addTextField(configurationHandler: { (textField) in
-                textField.text = text
-                textField.placeholder = placeholder
-                textField.isSecureTextEntry = secureTextEntry
-                textField.keyboardType = keyboardType
-                _textField = textField
-            })
-            presenter?.present(alert, animated: true, completion: nil)
-            return alert
-        }
-        else {
-            let alert = UIAlertView(title: title, message: message, cancelButtonTitle: cancelButtonTitle, otherButtonTitles: nil != otherButtonTitle ? [otherButtonTitle!] : nil, delayActiveTime: 0, callback: nil)
-            alert.alertViewStyle = secureTextEntry ? .secureTextInput : .plainTextInput
-            if nil != alert.textField {
-                alert.textField!.text = text
-                alert.textField!.placeholder = placeholder
-                alert.textField!.keyboardType = keyboardType
-            }
-            _textField = alert.textField
-            alert.lbk_textInputCallback = callback
-            alert.delegate = alert
-            alert.show()
-            return alert
-        }
-    }
-    
-    fileprivate convenience init(title: String?, message: String?, cancelButtonTitle: String?, otherButtonTitles: [String]?, delayActiveTime: TimeInterval, callback: ((_ sender: AnyObject, _ buttonIndex: NSInteger) -> ())?) {
-        self.init(title: title, message: message, delegate: nil, cancelButtonTitle: cancelButtonTitle)
-        if nil != otherButtonTitles {
-            for otherButtonTitle in otherButtonTitles! {
-                self.addButton(withTitle: otherButtonTitle)
-            }
-        }
-        self.lbk_disableFirstOtherButton = 0 < delayActiveTime
-        self.lbk_callback = callback
-        self.delegate = self
-        self.show()
-        if 0 < delayActiveTime {
-            weak var w = self
-            DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + Double(Int64(delayActiveTime * Double(NSEC_PER_SEC))) / Double(NSEC_PER_SEC), execute: {
-                guard let strong = w else { return }
-                strong.dismiss(withClickedButtonIndex: 0, animated: false)
-                strong.lbk_disableFirstOtherButton = false
-                strong.show()
-            })
-        }
-    }
-
-    public var textField: UITextField? {
-        return self.textField(at: 0)
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        weak var w = alert
+        alert.addAction(UIAlertAction(title: cancelButtonTitle, style: .default, handler: { (action) in
+            guard let strong = w else { return }
+            if nil != callback { callback!(strong, 0, nil) }
+        }))
+        alert.addAction(UIAlertAction(title: otherButtonTitle, style: .default, handler: { (action) in
+            guard let strong = w else { return }
+            if nil != callback { callback!(strong, 1, strong.textField?.text) }
+        }))
+        alert.addTextField(configurationHandler: { (textField) in
+            textField.text = text
+            textField.placeholder = placeholder
+            textField.isSecureTextEntry = secureTextEntry
+            textField.keyboardType = keyboardType
+            _textField = textField
+        })
+        presenter.present(alert, animated: true, completion: nil)
+        return alert
     }
 
     //MARK:- Private Properties
-    
-    fileprivate var lbk_disableFirstOtherButton: Bool {
-        get {
-            return objc_getAssociatedObject(self, &UIAlertViewDisableFirstOtherButtonKey) as? Bool ?? false
-        }
-        set {
-            objc_setAssociatedObject(self, &UIAlertViewDisableFirstOtherButtonKey, newValue, objc_AssociationPolicy.OBJC_ASSOCIATION_RETAIN_NONATOMIC)
-        }
-    }
     
     fileprivate var lbk_callback: UIAlertViewCallback? {
         get {
@@ -229,22 +165,6 @@ extension UIAlertView: UIAlertViewDelegate {
                 setHandler(handler: newValue!)
             }
         }
-    }
-
-    //MARK:- UIAlertViewDelegate
-    
-    public func alertView(_ alertView: UIAlertView, clickedButtonAt buttonIndex: Int) {
-        if nil != alertView.lbk_callback {
-            alertView.lbk_callback!(alertView, buttonIndex)
-        }
-        if nil != alertView.lbk_textInputCallback {
-            alertView.lbk_textInputCallback!(alertView, buttonIndex, nil != alertView.textField ? alertView.textField!.text : nil)
-        }
-
-    }
-    
-    public func alertViewShouldEnableFirstOtherButton(_ alertView: UIAlertView) -> Bool {
-        return !alertView.lbk_disableFirstOtherButton
     }
 }
 
